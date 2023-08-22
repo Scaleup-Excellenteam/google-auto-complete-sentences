05448145d9271f7994542edb49d0f78b15178265
antonnahhas <anton-nahhas@hotmail.com>
2023-08-22 15:50:41 +0300

    implemented a working CLI suitable for the user.

--- autocomplete.py
+++ autocomplete.py
@@ -1,5 +1,39 @@
+WELCOME_MSG = "****************** WELCOME TO OUR SEARCH SYSTEM ******************"
+GET_INPUT = "Enter a sentence (or # to restart, ~ to quit):"
+QUIT_MESSAGE = "~"
+RESTART_MESSAGE = "#"
+MATCHES = "Matches:"
+NO_MATCHES = "No matches found."
+
+
+def display_matches(sentence):
+    return ["damn", "this", "is", "working", "well!"]
+
+
+def run_CLI():
+    print(WELCOME_MSG)
+    sentence = ""
+    while True:
+        additional_input = input(f"{GET_INPUT} {sentence}")
+        sentence += additional_input
+
+        if sentence.endswith(QUIT_MESSAGE):
+            break
+
+        if sentence.endswith(RESTART_MESSAGE):
+            sentence = ""
+
+        matches = display_matches(sentence)
+        if matches:
+            print(MATCHES)
+            for match in matches:
+                print(f"- {match}")
+        else:
+            print(NO_MATCHES)
+
+
 def main():
-    print("Yo!")
+    run_CLI()
 
 
 if __name__ == "__main__":
