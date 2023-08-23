58194efabe93e2daff82823d9739ca2a731befe2
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-08-23 15:41:27 +0300

    save pickle data stracture

---
+++ new_main.py
@@ -0,0 +1,12 @@
+import pickle
+import cli as cli
+
+def main():
+    # Load the PrefixTree object from the pickle file
+    with open('prefix_tree.pkl', 'rb') as f:
+        tree = pickle.load(f)
+
+    cli.run_cli(tree)
+
+if __name__ == "__main__":
+    main()
